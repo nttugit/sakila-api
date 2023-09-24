f0ce4d3fbc0efb0c8265c134c29ac3ba2987883f
nttu <nttugpt@gmail.com>
2023-09-24 12:23:44 +0700

    Add validation for category & film, using AJV

--- routes/category.route.js
+++ routes/category.route.js
@@ -1,15 +1,16 @@
 import express from 'express';
+import { readFile } from 'fs/promises';
 const router = express.Router();
 import categoryHandler from '../handlers/category.handler.js';
+import validate from '../middlewares/validate.mdw.js';
+const schema = JSON.parse(
+    await readFile(new URL('../schemas/category.json', import.meta.url)),
+);
 
 router.get('/', categoryHandler.getListCategories);
-
 router.get('/:id', categoryHandler.getCategoryById);
-
-router.post('/', categoryHandler.postCategory);
-
-router.patch('/:id', categoryHandler.patchCategory);
-
+router.post('/', validate(schema), categoryHandler.postCategory);
+router.patch('/:id', validate(schema), categoryHandler.patchCategory);
 router.delete('/:id', categoryHandler.deleteCategory);
 
 export default router;
--- routes/film.route.js
+++ routes/film.route.js
@@ -1,14 +1,20 @@
 import express from 'express';
+import { readFile } from 'fs/promises';
 const router = express.Router();
 import filmHandler from '../handlers/film.handler.js';
+import validate from '../middlewares/validate.mdw.js';
+
+const schema = JSON.parse(
+    await readFile(new URL('../schemas/film.json', import.meta.url)),
+);
 
 router.get('/', filmHandler.getFilms);
 
 router.get('/:id', filmHandler.getFilmById);
 
-router.post('/', filmHandler.postFilm);
+router.post('/', validate(schema), filmHandler.postFilm);
 
-router.patch('/:id', filmHandler.patchFilm);
+router.patch('/:id', validate(schema), filmHandler.patchFilm);
 
 router.delete('/:id', filmHandler.deleteFilm);
 
